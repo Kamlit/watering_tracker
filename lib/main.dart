8fe63429670f463177e4c7cea1daef908d121dc9
Kamlit <kamlit2002@wp.pl>
2023-02-06 21:07:40 +0100

    main function seperate

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
-import 'package:drink_tracker/logic/blocs/authentication_bloc.dart';
 import 'package:drink_tracker/logic/blocs/authentication_observer.dart';
-import 'package:drink_tracker/logic/repositories/authorization_repository.dart';
-import 'package:drink_tracker/logic/routes.dart';
-import 'package:drink_tracker/presentation/screens/home/home_screen.dart';
-import 'package:drink_tracker/presentation/screens/login/login_screen.dart';
-import 'package:drink_tracker/presentation/screens/loading/loading_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:drink_tracker/presentation/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -15,38 +9,5 @@ Future<void> main() async {
   await Firebase.initializeApp();
   Paint.enableDithering = true;
   Bloc.observer = AuthenticationObserver();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return RepositoryProvider(
-      create: (context) => AuthorizationRepository(),
-      child: BlocProvider(
-        create: (context) => AuthenticationBloc(
-          authorizationRepository:
-              RepositoryProvider.of<AuthorizationRepository>(context),
-        )..add(AppStarted()),
-        child: MaterialApp(
-          title: 'Drinking Tracker',
-          home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
-            builder: (context, state) {
-              if(state is Authenticated) {
-                return const HomeScreen();
-              } else if (state is Unauthenticated) {
-                return const LoginScreen();
-              } else if(state is AuthenticationError) {
-                return const Center(child: Text('Error'));
-              }
-              return const LoadingScreen();
-            },
-          ),
-          routes: Routes.routes,
-        ),
-      ),
-    );
-  }
+  runApp(const App());
 }
---
+++ lib/presentation/app.dart
@@ -0,0 +1,41 @@
+import 'package:drink_tracker/logic/blocs/authentication_bloc.dart';
+import 'package:drink_tracker/logic/repositories/authorization_repository.dart';
+import 'package:drink_tracker/logic/routes.dart';
+import 'package:drink_tracker/presentation/screens/home/home_screen.dart';
+import 'package:drink_tracker/presentation/screens/loading/loading_screen.dart';
+import 'package:drink_tracker/presentation/screens/login/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider(
+      create: (context) => AuthorizationRepository(),
+      child: BlocProvider(
+        create: (context) => AuthenticationBloc(
+          authorizationRepository:
+              RepositoryProvider.of<AuthorizationRepository>(context),
+        )..add(AppStarted()),
+        child: MaterialApp(
+          title: 'Drinking Tracker',
+          home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+            builder: (context, state) {
+              if(state is Authenticated) {
+                return const HomeScreen();
+              } else if (state is Unauthenticated) {
+                return const LoginScreen();
+              } else if(state is AuthenticationError) {
+                return const Center(child: Text('Error'));
+              }
+              return const LoadingScreen();
+            },
+          ),
+          routes: Routes.routes,
+        ),
+      ),
+    );
+  }
+}
